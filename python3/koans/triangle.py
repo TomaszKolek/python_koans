3be2ee4a1cca0262d5eec13e235a5eb721302d5d
Tomek <email@email.com>
2013-11-21 21:42:08 +0100

    added raising errors

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,14 +17,20 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        raise TriangleError()
     if a == b == c:
-    	return 'equilateral'
+        if a == 0:
+            raise TriangleError()
+        return 'equilateral'
 
     elif a != b and a != c and b != c:
-    	return 'scalene'
+        return 'scalene'
 
     else:
-    	return 'isosceles'
+        if sum(sorted([a, b, c])[0:2]) <= max(a, b, c):
+            raise TriangleError
+        return 'isosceles'
     pass
 
 # Error class used in part 2.  No need to change this code.
